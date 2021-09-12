f121f6fc2027e50de0996052c9c139e0be503d15
NikolaMihovilovic <89147102+NikolaMihovilovic@users.noreply.github.com>
2021-09-12 19:45:02 +0200

    Update Zadatak2.sql

--- Nedelja6/Nedelja/Zadatak2.sql
+++ Nedelja6/Nedelja/Zadatak2.sql
@@ -1,1 +1,3 @@
-
+SELECT dependents.first_name, dependents.last_name FROM dependents
+INNER JOIN employees ON dependents.dependent_id = employees.department_id
+WHERE salary IN (SELECT MAX(salary) FROM employees)
