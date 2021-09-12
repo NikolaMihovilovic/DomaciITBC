be9a0a3d411a0538ca90edfd637dacf1ac170c14
NikolaMihovilovic <89147102+NikolaMihovilovic@users.noreply.github.com>
2021-09-12 20:07:51 +0200

    Update Zadatak1.sql

--- Nedelja6/Nedelja/Zadatak1.sql
+++ Nedelja6/Nedelja/Zadatak1.sql
@@ -1,1 +1,6 @@
-
+SELECT employees.email, employees.phone_number FROM employees
+INNER JOIN departments ON employees.department_id = departments.department_id
+INNER JOIN locations ON departments.location_id = locations.location_id
+INNER JOIN countries ON locations.country_id = countries.country_id
+INNER JOIN regions ON countries.region_id = regions.region_id
+WHERE regions.region_id = 1 AND employees.phone_number IS NOT NULL;
