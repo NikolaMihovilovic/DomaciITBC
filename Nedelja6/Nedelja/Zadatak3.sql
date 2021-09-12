0fc49c53a1cdbc5137037d6d3a5b3a5f7a4bd59c
NikolaMihovilovic <89147102+NikolaMihovilovic@users.noreply.github.com>
2021-09-12 19:15:56 +0200

    Update Zadatak3.sql

--- Nedelja6/Nedelja/Zadatak3.sql
+++ Nedelja6/Nedelja/Zadatak3.sql
@@ -1,1 +1,6 @@
-
+SELECT country_name FROM countries
+INNER JOIN locations ON countries.country_id = locations.country_id
+INNER JOIN departments ON locations.location_id = departments.location_id
+INNER JOIN employees ON departments.department_id = employees.department_id
+GROUP BY countries.country_name
+ORDER BY SUM (salary) DESC;
